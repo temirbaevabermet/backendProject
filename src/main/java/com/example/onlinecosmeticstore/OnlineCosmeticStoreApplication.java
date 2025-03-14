10116a10487fc1773bfe6570e3f2d22e5b2b035e
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-03-14 15:13:21 +0600

    Spring Initialized

---
+++ src/main/java/com/example/onlinecosmeticstore/OnlineCosmeticStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecosmeticstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCosmeticStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCosmeticStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinecosmeticstore/OnlineCosmeticStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecosmeticstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCosmeticStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
