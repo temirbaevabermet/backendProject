622ca9334efa4f4e71edbb6a159f17c05d0317f0
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-03-15 22:58:39 +0600

    A REST controller for Product has been developed to provide CRUD operations (GET, POST, PUT, PATCH, DELETE) for entities.

---
+++ src/main/java/com/example/onlinecosmeticstore/Controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.onlinecosmeticstore.Controller;
+
+import com.example.onlinecosmeticstore.Service.ProductService;
+import com.example.onlinecosmeticstore.dto.ProductDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) {
+        return ResponseEntity.ok(productService.createProduct(productDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        return ResponseEntity.ok(productService.updateProduct(id, productDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
