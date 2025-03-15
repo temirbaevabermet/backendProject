b7cda0d6e92b84b533f8f61e37f82247ac2ac718
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-03-15 23:04:17 +0600

    Service for Category was implemented,changes were also made to CategoryDTO

---
+++ src/main/java/com/example/onlinecosmeticstore/Service/CategoryService.java
@@ -0,0 +1,52 @@
+package com.example.onlinecosmeticstore.Service;
+
+import com.example.onlinecosmeticstore.Entity.Category;
+import com.example.onlinecosmeticstore.Repository.CategoryRepository;
+import com.example.onlinecosmeticstore.dto.CategoryDTO;
+import com.example.onlinecosmeticstore.mapper.CategoryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public CategoryDTO getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .map(categoryMapper::toDTO)
+                .orElseThrow(() -> new RuntimeException("Категория не найдена"));
+    }
+
+    public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toEntity(categoryDTO);
+        category = categoryRepository.save(category);
+        return categoryMapper.toDTO(category);
+    }
+
+    public CategoryDTO updateCategory(Long id, CategoryDTO categoryDTO) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Категория не найдена"));
+
+        category.setName(categoryDTO.getName());
+        category.setDescription(categoryDTO.getDescription());
+
+        category = categoryRepository.save(category);
+        return categoryMapper.toDTO(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
--- src/main/java/com/example/onlinecosmeticstore/dto/CategoryDTO.java
+++ src/main/java/com/example/onlinecosmeticstore/dto/CategoryDTO.java
@@ -1,8 +1,14 @@
 package com.example.onlinecosmeticstore.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
     private Long id;
     private String name;
