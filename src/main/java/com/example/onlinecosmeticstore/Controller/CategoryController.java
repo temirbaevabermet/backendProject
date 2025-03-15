51cf2ea2adb345e3cf184564745643ea58a3d488
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-03-15 23:10:49 +0600

    A REST controller for Category has been developed to provide CRUD operations (GET, POST, PUT, PATCH, DELETE) for entities.

---
+++ src/main/java/com/example/onlinecosmeticstore/Controller/CategoryController.java
@@ -0,0 +1,42 @@
+package com.example.onlinecosmeticstore.Controller;
+
+import com.example.onlinecosmeticstore.Service.CategoryService;
+import com.example.onlinecosmeticstore.dto.CategoryDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryDTO> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.createCategory(categoryDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, categoryDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
