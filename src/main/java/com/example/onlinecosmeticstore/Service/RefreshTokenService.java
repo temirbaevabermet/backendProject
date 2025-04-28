88799cb386d40e585e6c8d64c7f1d077dcbd6601
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-04-29 03:27:59 +0600

    Service for Refresh token was added

---
+++ src/main/java/com/example/onlinecosmeticstore/Service/RefreshTokenService.java
@@ -0,0 +1,49 @@
+package com.example.OnlineCosmeticStore.Service;
+
+import com.example.OnlineCosmeticStore.Entity.RefreshToken;
+import com.example.OnlineCosmeticStore.Entity.User;
+import com.example.OnlineCosmeticStore.Repository.RefreshTokenRepository;
+import com.example.OnlineCosmeticStore.Repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    @Value("${jwt.refreshExpirationMs}")
+    private Long refreshTokenDurationMs;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+
+    public RefreshToken createRefreshToken(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .user(user)
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(refreshTokenDurationMs))
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().isBefore(Instant.now())) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException("Refresh token was expired. Please make a new login request");
+        }
+        return token;
+    }
+
+    @Transactional
+    public int deleteByUserId(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return refreshTokenRepository.deleteByUser(user);
+    }
+}
