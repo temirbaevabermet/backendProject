b2a3eb8572f2cb72491f1b2d45235ba73431f70c
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-04-29 03:24:57 +0600

    Repository for Refresh token was added

---
+++ src/main/java/com/example/onlinecosmeticstore/Repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.OnlineCosmeticStore.Repository;
+
+import com.example.OnlineCosmeticStore.Entity.RefreshToken;
+import com.example.OnlineCosmeticStore.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    int deleteByUser(User user);
+}
