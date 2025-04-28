e838fbcaabdc8051fa9a5f52932f2738ef67f33c
temirbaevabermet <temirbaevabermet4@gmail.com>
2025-04-29 03:22:07 +0600

    Entity for Refresh token was added

---
+++ src/main/java/com/example/onlinecosmeticstore/Entity/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.example.OnlineCosmeticStore.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+}
